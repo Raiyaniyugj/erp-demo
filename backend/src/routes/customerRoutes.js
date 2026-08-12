34ee4475ee92ddfc049083268cb6c27875170e64
Raiyaniyugj <raiyaniyugj@users.noreply.github.com>
2026-08-12 10:07:03 +0530

    fix multer dest for vercel

--- backend/src/routes/customerRoutes.js
+++ backend/src/routes/customerRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: '/tmp' });
 const { createCustomer, getCustomers, getCustomer, updateCustomer, deleteCustomer, exportCustomers, importCustomers } = require('../controllers/customerController');
 const { protect } = require('../middlewares/authMiddleware');
 
--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ dest: '/tmp' });
 const { createProduct, getProducts, updateProduct, deleteProduct, exportProducts, importProducts } = require('../controllers/productController');
 const { protect, authorize } = require('../middlewares/authMiddleware');
 
